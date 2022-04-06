8b7e9d03b0f7a03811f732c8421d86fb0539087c
DEV-MAR2022\harri <powercommand2022@gmail.com>
2022-04-06 18:03:51 +0200

    refactoring, dokumentation

--- src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/DecryptionCommand.cs
+++ src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/DecryptionCommand.cs
@@ -1,19 +1,17 @@
 ﻿using PainKiller.PowerCommands.Core.BaseClasses;
+using PainKiller.PowerCommands.Core.Services;
 using PainKiller.PowerCommands.Shared.DomainObjects.Configuration;
 using PainKiller.PowerCommands.Shared.DomainObjects.Core;
 using PainKiller.PowerCommands.Shared.Enums;
 
 namespace PainKiller.PowerCommands.MyExampleCommands.Commands;
 
-public class DecryptCommand : EncryptionCommandBase
+public class DecryptCommand : CommandBase<CommandsConfiguration>
 {
-    public DecryptCommand(string identifier, CommandsConfiguration configuration) : base(identifier, configuration)
-    {
-    }
-
+    public DecryptCommand(string identifier, CommandsConfiguration configuration) : base(identifier, configuration) { }
     public override RunResult Run(CommandLineInput input)
     {
-        var decrypt = DecryptString(input.Arguments.First());
+        var decrypt = EncryptionService.Service.DecryptString(input.SingleArgument);
         WriteLine($"Input decryptet: {decrypt}", false);
         return CreateRunResult(this, input, RunResultStatus.Ok);
     }
--- src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/EncryptionCommand.cs
+++ src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/EncryptionCommand.cs
@@ -1,17 +1,17 @@
 ﻿using PainKiller.PowerCommands.Core.BaseClasses;
+using PainKiller.PowerCommands.Core.Services;
 using PainKiller.PowerCommands.Shared.DomainObjects.Configuration;
 using PainKiller.PowerCommands.Shared.DomainObjects.Core;
 using PainKiller.PowerCommands.Shared.Enums;
 
 namespace PainKiller.PowerCommands.MyExampleCommands.Commands;
 
-public class EncryptCommand : EncryptionCommandBase
+public class EncryptCommand : CommandBase<CommandsConfiguration>
 {
     public EncryptCommand(string identifier, CommandsConfiguration configuration) : base(identifier, configuration) { }
-
     public override RunResult Run(CommandLineInput input)
     {
-        var encrypt = EncryptString(input.Arguments.First());
+        var encrypt = EncryptionService.Service.EncryptString(input.SingleArgument);
         WriteLine($"Input encrypted: {encrypt}",false);
         return CreateRunResult(this, input, RunResultStatus.Ok);
     }
