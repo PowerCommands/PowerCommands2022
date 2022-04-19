09c1bc86a6926a4a3c9232cd4778b8e0f4041b72
DEV-MAR2022\harri <powercommand2022@gmail.com>
2022-04-20 00:02:39 +0200

    Add archive to zip file

--- src/PainKiller.PowerCommands/PainKiller.PowerCommands.Core/Commands/LogCommand.cs
+++ src/PainKiller.PowerCommands/PainKiller.PowerCommands.Core/Commands/LogCommand.cs
@@ -1,4 +1,5 @@
-﻿using PainKiller.PowerCommands.Core.BaseClasses;
+﻿using System.IO.Compression;
+using PainKiller.PowerCommands.Core.BaseClasses;
 using PainKiller.PowerCommands.Shared.Attributes;
 using PainKiller.PowerCommands.Shared.DomainObjects.Configuration;
 using PainKiller.PowerCommands.Shared.DomainObjects.Core;
@@ -6,8 +7,8 @@ using PainKiller.PowerCommands.Shared.Enums;
 
 namespace PainKiller.PowerCommands.Core.Commands;
 
-[PowerCommand(description: "View and manage the log", arguments: "action: show, delete, list", qutes:"filename: name of the file to be shown or deleted")]
-[Tags("core|diagnostic|log|debug")]
+[PowerCommand(description: "View and manage the log", arguments: "action: view, archive, list", qutes:"filename: name of the file to be viewed")]
+[Tags("core|diagnostic|log|debug|zip|compression")]
 public class LogCommand : CommandBase<CommandsConfiguration>
 {
     public LogCommand(string identifier, CommandsConfiguration configuration) : base(identifier, configuration) { }
@@ -15,6 +16,7 @@ public class LogCommand : CommandBase<CommandsConfiguration>
     public override RunResult Run(CommandLineInput input)
     {
         if (input.SingleArgument == "list") List();
+        if (input.SingleArgument == "archive") Archive();
         return CreateRunResult(this, input, RunResultStatus.Ok);
     }
 
@@ -22,6 +24,25 @@ public class LogCommand : CommandBase<CommandsConfiguration>
     {
         var dir = new DirectoryInfo(Configuration.Log.FilePath);
         foreach (var file in dir.GetFiles("*.log").OrderByDescending(f => f.LastWriteTime)) WriteLine($"{file.Name} {file.LastWriteTime}");
-        
+    }
+
+    private void Archive()
+    {
+        var d = DateTime.Now;
+        var fileStamp = $"archive{d.Year}{d.Month}{d.Day}{d.Hour}{d.Minute}";
+        var tempDirectory = $"{Configuration.Log.FilePath}\\{fileStamp}";
+        var zipFileName = $"{Configuration.Log.FilePath}\\{fileStamp}.zip";
+
+        var fileNames = Directory.GetFiles(Configuration.Log.FilePath, "*.log");
+
+        Directory.CreateDirectory(tempDirectory);
+
+        foreach (var fileName in fileNames)
+        {
+            var file = new FileInfo(fileName);
+            File.Copy(fileName, $"{tempDirectory}\\{file.Name}");
+            WriteLine($"{fileName} moved to archive directory {tempDirectory}");
+        }
+        ZipFile.CreateFromDirectory(tempDirectory, zipFileName);
     }
 }
