e02c995312700acff3f7fba42de3f9b4bd22deca
DEV-MAR2022\harri <powercommand2022@gmail.com>
2022-08-10 16:11:25 +0200

    .

---
+++ src/PainKiller.PowerCommands/Implementations/Drontlog-Implementation/PainKiller.PowerCommands.DrontlogCommands/Commands/DilbertCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using PainKiller.PowerCommands.DrontlogCommands.Extensions;
+
+namespace PainKiller.PowerCommands.DrontlogCommands.Commands;
+
+public class DilbertCommand : CommandBase<PowerCommandsConfiguration>
+{
+    public DilbertCommand(string identifier, PowerCommandsConfiguration configuration) : base(identifier, configuration) { }
+
+    public override RunResult Run(CommandLineInput input)
+    {
+        var url = $"https://dilbert.com/strip/{DateTime.Now.ToDateString()}";
+        using var webClient = new WebClient { BaseAddress =  url};
+        var find = "  <img class=\"img-responsive img-comic\"";
+        
+        var content = webClient.DownloadString(url);
+        var rows = content.Split('\n');
+        var row = rows.FirstOrDefault(r => r.Contains(find));
+
+        if (row == null) return CreateBadParameterRunResult(input, "No data found");
+        
+        // <img class="img-responsive img-comic" width="900" height="280" alt="Keep The Plastic Bag - Dilbert by Scott Adams" src="https://assets.amuniversal.com/ed7c0900f179013aba2a005056a9545d" />
+        var imageLink = row.Split("src=")[1];
+        imageLink = imageLink.Replace("\"", "").Replace(" />","");
+        var htmlContent = $"<img src=\"{imageLink}\" alt=\"Dilbert daily\"/>";
+
+        WriteLine(htmlContent);
+
+        return CreateRunResult(input);
+    }
+}
---
+++ src/PainKiller.PowerCommands/Implementations/Drontlog-Implementation/PainKiller.PowerCommands.DrontlogCommands/Extensions/FormatExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace PainKiller.PowerCommands.DrontlogCommands.Extensions;
+
+public static class FormatExtensions
+{
+    public static string ToDateString(this DateTime dateTime) => $"{dateTime.Year}-{dateTime.Month.ToString().PadLeft(2,'0')}-{dateTime.Day.ToString().PadLeft(2, '0')}";
+}
