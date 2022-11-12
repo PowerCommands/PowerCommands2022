0d1c791e9bf9c52251ba1ffd4690d3c3d93d29b2
Harri Klingsten <powercommand2022@gmail.com>
2022-11-12 21:22:25 +0100

    Added publish to release

--- src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/ReleaseCommand.cs
+++ src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/ReleaseCommand.cs
@@ -3,57 +3,38 @@
 namespace PainKiller.PowerCommands.MyExampleCommands.Commands;
 
 [PowerCommandDesign(description: "Command that prepares the implementation VS solutions with the new Core projects and then builds them to verify that the changes did not break anything",
-                          flags: "all|no-build|build-only|windows|knowledge|glitch|nist|web",
+                          flags: "all|no-build|build-only|publish|windows|knowledge|glitch|nist|web",
                         example: "//Release and build all|release --all|//Release and build Glitchfinder and NistNvd|release --glitch --nist|//Just replace Core without build|release --all --no-build|//Just build the implementations|release --all --build-only")]
 public class ReleaseCommand : CommandBase<PowerCommandsConfiguration>
 {
-    private List<BuildSummary> _summary = new();
+    private readonly List<BuildSummary> _summary = new();
     public ReleaseCommand(string identifier, PowerCommandsConfiguration configuration) : base(identifier, configuration) { }
 
     public override RunResult Run()
     {
         if (!Input.MustOnOfTheseFlagsCheck(Input.Flags)) return BadParameterError("You must provide a least one flag option of these <all|windows|knowledge|glitch|nist>");
-
         _summary.Clear();
 
         var all = Input.HasFlag("all");
-        string solutionPath;
-        
-        if (Input.HasFlag("windows") || all)
+        var releases = new Release[]
         {
-            solutionPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", "Implementations\\Windows-implementation");
-            WriteHeadLine("Windows-implementation...");
-            if (Input.NoFlag("build-only")) UpdateCoreDirectory(solutionPath);
-            if(Input.NoFlag("no-build")) Build(solutionPath);
-        }
-        if (Input.HasFlag("knowledge") || all)
+            new(){Flag ="windows",Name = "Windows"},
+            new() { Flag = "knowledge", Name = "KnowledgeDB" },
+            new() { Flag = "glitch", Name = "GlitchFinder" },
+            new() { Flag = "nist", Name = "NistNvd" },
+            new() { Flag = "web", Name = "WebClient" }
+        };
+        foreach (var release in releases)
         {
-            WriteHeadLine("KnowledgeDB-implementation...");
-            solutionPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", "Implementations\\KnowledgeDB-Implementation");
-            if (Input.NoFlag("build-only")) UpdateCoreDirectory(solutionPath);
-            if (Input.NoFlag("no-build")) Build(solutionPath);
-        }
-        if (Input.HasFlag("glitch") || all)
-        {
-            WriteHeadLine("GlitchFinder-implementation...");
-            solutionPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", "Implementations\\GlitchFinder-Implementation");
+            if (!Input.HasFlag($"{release.Flag}") && !all) continue;
+            var solutionPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", $"Implementations\\{release.Name}-implementation");
+            WriteHeadLine($"{release.Name}-implementation...");
             if (Input.NoFlag("build-only")) UpdateCoreDirectory(solutionPath);
             if (Input.NoFlag("no-build")) Build(solutionPath);
         }
-        if (Input.HasFlag("nist") || all)
-        {
-            WriteHeadLine("NistNvd-Implementation...");
-            solutionPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", "Implementations\\NistNvd-Implementation");
-            if (Input.NoFlag("build-only")) UpdateCoreDirectory(solutionPath);
-            if (Input.NoFlag("no-build")) Build(solutionPath);
-        }
-        if (Input.HasFlag("web") || all)
-        {
-            WriteHeadLine("WebClient-Implementation...");
-            solutionPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", "Implementations\\WebClient-Implementation");
-            if (Input.NoFlag("build-only")) UpdateCoreDirectory(solutionPath);
-            if (Input.NoFlag("no-build")) Build(solutionPath);
-        }
+        var projectPath = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", $"Implementations\\KnowledgeDB-Implementation\\PainKiller.PowerCommands.PowerCommandsConsole");
+        if (Input.HasFlag("publish")) Publish(projectPath);
+
         ConsoleTableService.RenderConsoleCommandTable(_summary, this);
         return Ok();
     }
@@ -68,6 +49,12 @@ public class ReleaseCommand : CommandBase<PowerCommandsConfiguration>
         IOService.CopyFolder(coreDirectory, coreImplementationDirectory);
         _summary.Add(new BuildSummary { Path = solutionPath, CoreProjectsUpdated = true });
     }
+    private void Publish(string solutionPath)
+    {
+        ShellService.Service.Execute("dotnet", "publish -p:PublishProfile=FolderProfile", solutionPath, WriteLine, "", waitForExit: true);
+        var success = !FindInOutput("Build FAILED");
+        AddOrUpdate(success ? new BuildSummary { Path = SolutionFileManager.GetLocalSolutionRoot().Replace("src\\PainKiller.PowerCommands\\", $"Implementations\\KnowledgeDB-implementation"), BuildStatus = "*OK*", Publish = "*OK*"} : new BuildSummary { Path = solutionPath, BuildStatus = "*FAILED*", Publish = "*FAILED*" });
+    }
     private void Build(string solutionPath)
     {
         ClearOutput();
@@ -75,20 +62,22 @@ public class ReleaseCommand : CommandBase<PowerCommandsConfiguration>
         var success = !FindInOutput("Build FAILED");
         AddOrUpdate(success ? new BuildSummary { Path = solutionPath, BuildStatus = "*OK*" } : new BuildSummary { Path = solutionPath, BuildStatus = "*FAILED*" });
     }
-
     private void AddOrUpdate(BuildSummary summary)
     {
         var existing = _summary.FirstOrDefault(s => s.Path == summary.Path);
         if (existing != null) existing.BuildStatus = summary.BuildStatus;
         else _summary.Add(summary);
     }
+    class Release { public string? Flag { get; set; } public string? Name { get; set; } }
     class BuildSummary : IConsoleCommandTable
     {
         [ColumnRenderOptions(caption: "Solution path", order: 1)]
         public string Path { get; set; } = "";
-        [ColumnRenderOptions(caption: "Core updated", order: 1)]
+        [ColumnRenderOptions(caption: "Core updated", order: 2)]
         public bool CoreProjectsUpdated { get; set; }
-        [ColumnRenderOptions(caption: "Status", order: 5, renderFormat: ColumnRenderFormat.SucessOrFailure, trigger1: "*OK*", trigger2: "*FAILED*", mark: "*")]
+        [ColumnRenderOptions(caption: "Status", order: 3, renderFormat: ColumnRenderFormat.SucessOrFailure, trigger1: "*OK*", trigger2: "*FAILED*", mark: "*")]
         public string BuildStatus { get; set; } = "";
+        [ColumnRenderOptions(caption: "Published", order: 4, renderFormat: ColumnRenderFormat.SucessOrFailure, trigger1: "*OK*", trigger2: "*FAILED*", mark: "*")]
+        public string Publish { get; set; } = "Disabled";
     }
 }
