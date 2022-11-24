cfa933af954c2f07a451cc0dd4ed56c440753ea0
Harri Klingsten <powercommand2022@gmail.com>
2022-11-24 23:28:15 +0100

    refactoring

--- src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/Templates/DefaultCommand.cs
+++ src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/Templates/DefaultCommand.cs
@@ -5,9 +5,9 @@ namespace PainKiller.PowerCommands.MyExampleCommands.Commands.Templates;
 [PowerCommandTest(         tests: " ")]
 [PowerCommandDesign( description: "Description of your command...",
                          example: "demo")]
-public class NameCommand : CommandBase<CommandsConfiguration>
+public class NameCommand : CommandBase<PowerCommandsConfiguration>
 {
-    public NameCommand(string identifier, CommandsConfiguration configuration) : base(identifier, configuration) { }
+    public NameCommand(string identifier, PowerCommandsConfiguration configuration) : base(identifier, configuration) { }
 
     public override RunResult Run()
     {
