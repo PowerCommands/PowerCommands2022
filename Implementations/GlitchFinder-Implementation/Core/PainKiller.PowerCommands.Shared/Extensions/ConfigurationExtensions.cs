d17fb45163d0446d067c2eeb4bc074c398c9a7cd
Harri Klingsten <powercommand2022@gmail.com>
2022-11-29 18:02:02 +0100

    refactoring

--- Implementations/GlitchFinder-Implementation/Core/PainKiller.PowerCommands.Shared/Extensions/ConfigurationExtensions.cs
+++ Implementations/GlitchFinder-Implementation/Core/PainKiller.PowerCommands.Shared/Extensions/ConfigurationExtensions.cs
@@ -4,7 +4,7 @@ namespace PainKiller.PowerCommands.Shared.Extensions;
 
 public static class ConfigurationExtensions
 {
-    public static void AddEnvironmentVariable(this EnvironmentConfiguration configuration, string name, string val, EnvironmentVariableTarget target)
+    public static void AddEnvironmentVariable(this EnvironmentConfiguration configuration, string name, string val, EnvironmentVariableTarget target = EnvironmentVariableTarget.User)
     {
         if (configuration.Variables != null) configuration.Variables.Add(new EnvironmentItemConfiguration { EnvironmentVariableTarget = $"{target}", Name = name });
         else configuration.Variables = new List<EnvironmentItemConfiguration> { new() { EnvironmentVariableTarget = $"{EnvironmentVariableTarget.User}", Name = name } };
