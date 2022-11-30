31111877ef85cde7a292695df7bb13bc5834b3dc
Harri Klingsten <powercommand2022@gmail.com>
2022-11-30 12:25:42 +0100

    refactoring

--- src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/GitCommand.cs
+++ src/PainKiller.PowerCommands/PainKiller.PowerCommands.MyExampleCommands/Commands/GitCommand.cs
@@ -1,9 +1,10 @@
 ﻿namespace PainKiller.PowerCommands.MyExampleCommands.Commands;
 
 [PowerCommandDesign(description: "Example shows how to execute a external program, in this case git, commit and push your repository, path to repository is in the configuration file",
-                arguments: "!commit|push|status",
-                    quotes: "\"<comment>\" defaults to \"refactoring\" if omitted, only used with commit.",
-                  example: "//Add and commit|git commit \"Bugfix\"|//Performs a push to Git repo|git push|//Git status of the configured git repo|git status|//Show local path to repo|git repo")]
+                arguments: "!commit|push|status|log|branch",
+                  options: "create|change|delete|merge|main",
+                   quotes: "\"<comment>\" defaults to \"refactoring\" if omitted, only used with commit.",
+                  example: "//Add and commit|git commit \"Bugfix\"|//Performs a push to Git repo|git push|//Git status of the configured git repo|git status|//Show log|git log|//Create and change to branch|git branch --create my-branch|//Change branch|git branch --change my-branch|//Merge branch|git --merge my-branch|//Delete branch locally (and remote if you want)|git --delete my-branch|//Change to main branch|git branch main")]
 
 public class GitCommand : CommandBase<PowerCommandsConfiguration>
 {
@@ -15,43 +16,66 @@ public class GitCommand : CommandBase<PowerCommandsConfiguration>
             case "commit":
                 Commit(Input.SingleQuote);
                 break;
+            case "branch":
+                if(HasOption("change")) RunSingleCommand("checkout",GetOptionValue("change"));
+                if(HasOption("create")) Create(GetOptionValue("create"));
+                if(HasOption("main")) RunSingleCommand("checkout","main");
+                if(HasOption("merge")) Merge(GetOptionValue("merge"));
+                if(HasOption("delete")) Delete(GetOptionValue("delete"));
+                break;
+            case "merge":
+                Merge(Input.SingleQuote);
+                break;
             case "push":
-                Push();
-                break;
             case "status":
-                Status();
-                break;
-            case "repo":
-                Repo();
+            case "log":
+                RunSingleCommand(Input.SingleArgument, $"{Input.SingleQuote}");
                 break;
             default: 
                 return BadParameterError($"Parameter {Input.SingleArgument} not supported");
         }
         return Ok();
     }
-    public void Commit(string comment)
+    private void Commit(string comment)
     {
         if (string.IsNullOrEmpty(comment)) comment = "\"refactoring\"";
-        ShellService.Service.Execute("git", "add .", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
-        WriteProcessLog("GIT", $"add . \"{comment}\"");
+        RunSingleCommand("add .");
         ShellService.Service.Execute("git", $"commit -m \"{comment}\"", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
         WriteProcessLog("GIT", $"commit m \"{comment}\"");
     }
-    public void Push()
-    {
-        ShellService.Service.Execute("git", "push", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
-        WriteProcessLog("GIT", "push");
-    }
-
-    public void Status()
+    private void RunSingleCommand(string command, string name = "")
     { 
-        WriteHeadLine($"Current Git repository: {Configuration.DefaultGitRepositoryPath}\n");
-        ShellService.Service.Execute("git", "status", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
-        WriteProcessLog("GIT", $"status"); }
-
-    public void Repo()
+        WriteHeadLine($"Local repo path: {Configuration.DefaultGitRepositoryPath}\n");
+        ShellService.Service.Execute("git", $"{command} {name}", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
+        WriteProcessLog("GIT", $"{command} {name}");
+    }
+    private void Merge(string branchName)
     {
-        WriteLine($"Local repo path: {Configuration.DefaultGitRepositoryPath}");
-        WriteProcessLog("GIT", $"Local repo path: {Configuration.DefaultGitRepositoryPath}");
+        RunSingleCommand("checkout","main");
+        ShellService.Service.Execute("git", $"merge \"{branchName}\"", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
+        WriteProcessLog("GIT", $"merge \"{branchName}\"");
+    }
+    private void Delete(string branchName)
+    {
+        RunSingleCommand("checkout","main");
+
+        //Locally
+        ShellService.Service.Execute("git", $"branch \"{branchName}\" -D", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
+        WriteProcessLog("GIT", $"branch \"{branchName}\" -D");
+        var deleteRemote = DialogService.YesNoDialog("Do you also want to delete the branch remote (on the server)?");
+        if (!deleteRemote) return;
+        //Remote (server)
+        ShellService.Service.Execute("git", $"push origin --delete \"{branchName}\"", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
+        WriteProcessLog("GIT", $"push origin --delete \"{branchName}\"");
+    }
+    private void Create(string branchName)
+    {
+        RunSingleCommand("branch", branchName);
+        
+        ShellService.Service.Execute("git", $"checkout \"{branchName}\"", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
+        WriteProcessLog("GIT", $"checkout \"{branchName}\"");
+
+        ShellService.Service.Execute("git", $"push --set-upstream origin \"{branchName}\"", Configuration.DefaultGitRepositoryPath, WriteLine, waitForExit: true);
+        WriteProcessLog("GIT", $"push --set-upstream origin \"{branchName}\"");
     }
 }
